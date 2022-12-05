bc09b9cc3fa273128e7d7dae07cc32dcb594b99f
MSI\tekit <tekitolisha8901@outlook.com>
2022-12-05 12:23:23 +0900

    BTEAM-43 [Update] 予測アイテムの透明度が徐々に下がるスクリプト追加

---
+++ DateApps2023/Assets/Project/Scripts/Boss/PredictItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredictItem : MonoBehaviour
+{
+    MeshRenderer myMesh;
+
+    [SerializeField]
+    private float transparentSpeed = 0.01f;
+
+    [SerializeField]
+    private float transparentValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = this.gameObject.GetComponent<MeshRenderer>();
+        myMesh.material.color = new Color(myMesh.material.color.r, myMesh.material.color.g, myMesh.material.color.b, transparentValue);
+        StartCoroutine("Transparent");
+    }
+
+   IEnumerator Transparent()
+    {
+        for (int i = 0; i < 255 - transparentValue; i++)
+        {
+            myMesh.material.color = myMesh.material.color + new Color32(0, 0, 0, 1);
+            yield return new WaitForSeconds(transparentSpeed);
+        }
+    }
+}
