0e1f48bfce159812e71f161546f5b3822db5dccb
MSI\jfuki <jfukiage46@gmail.com>
2022-12-24 17:42:55 +0900

    BTEAM-59 [Add] CannonSwitchクラス追加

---
+++ DateApps2023/Assets/Project/Scripts/Tower/CannonSwitch.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private EnergyCharge energyCharge = null;
+
+    [SerializeField]
+    private CannonShot cannonShot = null;
+
+    private BoxCollider boxCollider = null;
+
+    private bool isShot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isShot = !cannonShot.IsShotting && energyCharge.IsEnergyCharge();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (energyCharge.IsEnergyCharge())
+            {
+                cannonShot.Shot();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Punch"))
+        {
+            return;
+        }        
+
+        if (isShot)
+        {
+            cannonShot.Shot();
+        }
+    }
+}
