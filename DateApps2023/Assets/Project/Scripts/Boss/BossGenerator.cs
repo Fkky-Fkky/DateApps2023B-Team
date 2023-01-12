5cebb4521ecf9544de59c3c9a48f0feb26bf4d38
MSI\GAME＿武田碧＿PC <takedaaoi7@gmail.com>
2023-01-12 17:19:13 +0900

    BTEAM-76 [Update] ボスのランダム生成の完成

--- DateApps2023/Assets/Project/Scripts/Boss/BossGenerator.cs
+++ DateApps2023/Assets/Project/Scripts/Boss/BossGenerator.cs
@@ -21,9 +21,9 @@ public class BossGenerator : MonoBehaviour
 
     int bossPattern;
 
+    int patternNumber;
     int start = 1;
-    int end   = 3;
-    List<int> pattern = new List<int>();
+    int end   = 4;
 
     void Start()
     {
@@ -39,11 +39,25 @@ public class BossGenerator : MonoBehaviour
         }
     }
 
+    int GetRandomValue(int oldnum)
+    {
+        patternNumber = Random.Range(start, end);
+
+        if (patternNumber == oldnum)
+        {
+            int n = patternNumber + Random.Range(1, end);
+
+            return n < end ? n : n - end;
+        }
+        else
+        {
+            return patternNumber;
+        }
+    }
+
     void BossRandomGeneration()
     {
-            bossPattern = Random.Range(1,3);
-
-       
+        bossPattern = GetRandomValue(bossPattern);
 
         switch (bossPattern) {
             case 1:
@@ -62,7 +76,42 @@ public class BossGenerator : MonoBehaviour
                 bossR.transform.position = bossPositionRight;
                 intervalTime = 0.0f;
                 break;
+            case 4:
+                bossC = Instantiate(bossModel);
+                bossC.transform.position = bossPositionCenter;
 
+                bossL = Instantiate(bossModel);
+                bossL.transform.position = bossPositionLeft;
+                intervalTime = 0.0f;
+                break;
+            case 5:
+                bossR = Instantiate(bossModel);
+                bossR.transform.position = bossPositionRight;
+
+                bossL = Instantiate(bossModel);
+                bossL.transform.position = bossPositionLeft;
+                intervalTime = 0.0f;
+                break;
+            case 6:
+                bossC = Instantiate(bossModel);
+                bossC.transform.position = bossPositionCenter;
+
+                bossR = Instantiate(bossModel);
+                bossR.transform.position = bossPositionRight;
+                intervalTime = 0.0f;
+                break;
+            case 7:
+                bossC = Instantiate(bossModel);
+                bossC.transform.position = bossPositionCenter;
+
+                bossR = Instantiate(bossModel);
+                bossR.transform.position = bossPositionRight;
+
+                bossL = Instantiate(bossModel);
+                bossL.transform.position = bossPositionLeft;
+                intervalTime = 0.0f;
+
+                break;
         }
 
     }
