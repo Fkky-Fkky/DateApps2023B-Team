57b9ead4ef4adbe241c0892ae325c251fef0e6e1
MSI\jfuki <jfukiage46@gmail.com>
2023-01-23 12:29:06 +0900

    BTEAM-90 [Add] CannonDamageクラス追加

---
+++ DateApps2023/Assets/Project/Scripts/Cannon/CannonDamage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonDamage : MonoBehaviour
+{
+    [SerializeField]
+    EnergyCharge energyCharge = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("BossAttack"))
+        {
+            return;
+        }
+        energyCharge.DisChargeEnergy();
+    }
+}
