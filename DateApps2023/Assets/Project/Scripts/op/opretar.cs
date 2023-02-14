7619ff535b2d919d530701a01a381bfe80dd59f9
maruko ryo <MSI\gazan>
2023-02-14 15:15:01 +0900

    BTEAM-135 [Update] オペレーターの機能追加

---
+++ DateApps2023/Assets/Project/Scripts/op/opretar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opretar : MonoBehaviour
+{
+    Animator animator;
+
+    enum summon
+    {
+        stert,
+
+        idle,
+
+       
+
+        end,
+    }
+    summon gameState = summon.stert;
+
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+
+    
+}
