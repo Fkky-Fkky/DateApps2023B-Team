226c6b3c74977859f522812bf3182f7714c2f0e7
MSI\tekit <tekitolisha8901@outlook.com>
2023-02-24 17:54:23 +0900

    BTEAM-225 [Update] クリア時とゲームオーバー時にボイス追加

--- DateApps2023/Assets/Project/Scripts/Scene/SceneMove.cs
+++ DateApps2023/Assets/Project/Scripts/Scene/SceneMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,41 +22,72 @@ public class SceneMove : MonoBehaviour
 
     private float time = 0.0f;
 
+    [SerializeField]
+    private AudioClip sceneVoice = null;
+    private AudioSource audioSource;
+
+    private bool isPlaying = false;
+
+    [SerializeField]
+    private float beforeVoiceTime = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         SceneChangeFlag = false;
         IsAnimation = false;
         time = 0.0f;
+        if(sceneVoice != null )
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.volume = 1.0f;
+            isPlaying = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         if (!SceneChangeFlag)
         {
+            if (isPlaying)
+            {
+                time += Time.deltaTime;
+                if (time >= beforeVoiceTime)
+                {
+                    audioSource.PlayOneShot(sceneVoice);
+                    isPlaying = false;
+                }
+            }
             for (int i = 0; i < Gamepad.all.Count; i++)
             {
                 var gamepad = Gamepad.all[i];
                 if (gamepad.bButton.wasPressedThisFrame)
                 {
                     SceneChangeFlag = true;
+                    time = 0.0f;
                 }
             }
         }
         else
         {
+            time += Time.deltaTime;
+
             if (AnimationImage != null)
             {
                 IsAnimation = true;
                 AnimationImage.SetTrigger("AcceptStart");
-                time += Time.deltaTime;
                 if (time >= AfterPressTime)
                 {
                     IsAnimation = false;
                     time = 0.0f;
                 }
+            }
+            if(sceneVoice != null)
+            {
+                audioSource.volume = (float)(1.0 - time / AfterPressTime);
+
             }
             if (!IsAnimation)
             {
