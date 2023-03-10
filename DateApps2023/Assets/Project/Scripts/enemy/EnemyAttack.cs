000906423c1cc8bec9e91ca51777e726e07ad14f
maruko ryo <MSI\gazan>
2023-03-10 13:01:34 +0900

    BTEAm-249 [Update] コードの名前変え忘れを修正

--- DateApps2023/Assets/Project/Scripts/enemy/EnemyAttack.cs
+++ DateApps2023/Assets/Project/Scripts/enemy/EnemyAttack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     [SerializeField]
-    private Enemy1 enemy;
+    private Enemy enemy;
     void OnTriggerEnter(Collider collision)//Trigger
     {
         if (collision.gameObject.CompareTag("Player"))
