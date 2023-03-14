8b317661db44ce7b8706d3a6e9c4d9e7402106ea
MSI\GAME＿武田碧＿PC <takedaaoi7@gmail.com>
2023-03-14 17:10:05 +0900

    BTEAM-256 [Update] マジックナンバー撲滅

--- DateApps2023/Assets/Project/Scripts/Boss/VFX/TargetV3D.cs
+++ DateApps2023/Assets/Project/Scripts/Boss/VFX/TargetV3D.cs
@@ -8,6 +8,11 @@ public class TargetV3D : MonoBehaviour
     public Transform targetCursor;
     public float speed = 1f;
 
+    const float CENTER_POS = 0.0f;
+    const float RIGHT_POS  = 1.0f;
+    const float LEFT_POS   = -0.1f;
+
+
     //private Vector3 mouseWorldPosition;
 
     [SerializeField]
@@ -24,21 +29,21 @@ public class TargetV3D : MonoBehaviour
     // Positioning cursor prefab
     void FixedUpdate()
     {
-        if (bossPosition.position.x == 0.0f)
+        if (bossPosition.position.x == CENTER_POS)
         {
             Quaternion toRotation = Quaternion.LookRotation(targetCenter.position - transform.position);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
             targetCursor.position = targetCenter.position;
         }
 
-        if (bossPosition.position.x >= 0.1f)
+        if (bossPosition.position.x >= RIGHT_POS)
         {
             Quaternion toRotation = Quaternion.LookRotation(targetRight.position - transform.position);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
             targetCursor.position = targetRight.position;
         }
 
-        if (bossPosition.position.x <= -0.1f)
+        if (bossPosition.position.x <= LEFT_POS)
         {
             Quaternion toRotation = Quaternion.LookRotation(targetLeft.position - transform.position);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
