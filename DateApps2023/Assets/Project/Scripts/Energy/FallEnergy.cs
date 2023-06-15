2a70cc1471e4f28bbc1d0907bc622ac1ab04b951
jfuki <jfukiage46@gmail.com>
2023-06-15 13:52:05 +0900

    BTEAM-282
    [Update]
    transformをキャッシュしてから使うようにした

--- DateApps2023/Assets/Project/Scripts/Energy/FallEnergy.cs
+++ DateApps2023/Assets/Project/Scripts/Energy/FallEnergy.cs
@@ -9,23 +9,22 @@ namespace Resistance
     public class FallEnergy : MonoBehaviour
     {
         private bool isEnergyResourceLanded = false;
-        private Vector3 position = Vector3.zero;
+        private Transform transformCache = null;
 
         private const float FALL_SPEED = 15.0f;
+
+        private void Awake()
+        {
+            transformCache = transform;
+        }
 
         private void OnEnable()
         {
             const float SRART_POSITION_Y = 20.0f;
             isEnergyResourceLanded = false;
-            position = transform.position;
+            Vector3 position = transformCache.position;
             position.y = SRART_POSITION_Y;
-            transform.position = position;
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            position = transform.position;
+            transformCache.position = position;
         }
 
         // Update is called once per frame
@@ -43,9 +42,10 @@ namespace Resistance
         /// </summary>
         private void Fall()
         {
+            Vector3 position = transformCache.position;
             position.y = Mathf.Max(position.y - FALL_SPEED * Time.deltaTime, 0.0f);
-            transform.position = position;
-            if (transform.position.y <= 0.0f)
+            transformCache.position = position;
+            if (position.y <= 0.0f)
             {
                 isEnergyResourceLanded = true;
             }
